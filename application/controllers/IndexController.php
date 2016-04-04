456194f789cde4e79807adcfbb9fd6d1f2067d0f
mervat <mervatmosaad96@gmail.com>
2016-04-04 09:33:25 +0200

    choice country and list all cities

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -19,6 +19,9 @@ class IndexController extends Zend_Controller_Action
          
         $country_model= new Application_Model_Country();
         $this->view->countries=$country_model->list_All_Countries();
+        $layout = Zend_Layout::getMvcInstance();
+        $view = $layout->getView();
+        $view->whatever = 'foo';
          $city_model=new Application_Model_City();
         $this->view->cities= $city_model->listcities();
     }
@@ -27,7 +30,27 @@ class IndexController extends Zend_Controller_Action
 
 }
 
+/*
 
+class My_Layout_Plugin extends Zend_Controller_Plugin_Abstract
+{
+   public function preDispatch(Zend_Controller_Request_Abstract $request)
+   {
+      $layout = Zend_Layout::getMvcInstance();
+      $view = $layout->getView();
+
+      $view->whatever = 'foo';
+   }
+}
+
+
+
+then register this plugin with the front controller, e.g.
+
+Zend_Controller_Front::getInstance()->registerPlugin(new My_Layout_Plugin());
+
+
+*/
 
 
 
